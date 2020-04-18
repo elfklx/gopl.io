9fa56c5a49a46e33ccf37d4762fdcc18897ba3e1
Fei Yang <klxfeiyang@gmail.com>
2020-04-18 13:50:18 -0700

    format
    Authored-by: Fei Yang <fyang@pivotal.io>

--- ch7/eval/string_test.go
+++ ch7/eval/string_test.go
@@ -2,6 +2,7 @@ package eval_test
 
 import (
 	"math"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -9,21 +10,21 @@ import (
 )
 
 var _ = Describe("String", func() {
-   It("should print variable correctly", func() {
-		 v := Var("x")
-		 Expect(v.String()).To(Equal("x"))
-   })
+	It("should print variable correctly", func() {
+		v := Var("x")
+		Expect(v.String()).To(Equal("x"))
+	})
 
-	 It("should print an expression correctly", func() {
-		  expr, err := Parse("sin(pow(x, 3)) + sqrt(A / pi)")
-			Expect(err).To(Not(HaveOccurred()))
-			Expect(expr.String()).To(Equal("(sin(pow(x, 3)) + sqrt((A / pi)))"))
+	It("should print an expression correctly", func() {
+		expr, err := Parse("sin(pow(x, 3)) + sqrt(A / pi)")
+		Expect(err).To(Not(HaveOccurred()))
+		Expect(expr.String()).To(Equal("(sin(pow(x, 3)) + sqrt((A / pi)))"))
 
-			expr2, err := Parse(expr.String())
-			Expect(err).To(Not(HaveOccurred()))
-			Expect(expr2.String()).To(Equal("(sin(pow(x, 3)) + sqrt((A / pi)))"))
+		expr2, err := Parse(expr.String())
+		Expect(err).To(Not(HaveOccurred()))
+		Expect(expr2.String()).To(Equal("(sin(pow(x, 3)) + sqrt((A / pi)))"))
 
-			res := expr2.Eval(Env{"x": 8, "A": 3, "pi": math.Pi})
-			Expect(res).To(BeNumerically("~", 1.056, 0.01))
-	 })
+		res := expr2.Eval(Env{"x": 8, "A": 3, "pi": math.Pi})
+		Expect(res).To(BeNumerically("~", 1.056, 0.01))
+	})
 })
